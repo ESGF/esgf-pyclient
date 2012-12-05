25acf9e7f373c6da7e0a75b50786b36f95e1ac5c
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2012-12-05 02:00:32 +0000

    Utility script for removing queries from the cache

---
+++ uncache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+from test.config import CACHE_DIR
+from test.cache_urllib2 import remove_from_cache
+
+full_cache_dir = 'test/%s' % CACHE_DIR
+
+if __name__ == '__main__':
+    urls = sys.argv[1:]
+
+    for url in urls:
+        print 'Removing %s' % url
+        remove_from_cache(url, full_cache_dir)
+        
