967a80d0ad484d2fc0e363f3fb36836a5e1ad797
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2012-12-07 02:48:26 +0000

    New version v0.0.4

--- pyesgf/__init__.py
+++ pyesgf/__init__.py
@@ -3,7 +3,7 @@ Top-level package for Python interface to ESGF services.
 
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 #!TODO: ResultFormatter class.  process response json to specialise the result json.  Default is None
 #!TODO: pipe results to new process.  Command-line interface.
