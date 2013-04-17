12b68e5394b077fdde8c62c5104db66157827735
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-04-17 16:35:27 +0100

    Added a test for Issue #9 but it is not reproducing the problem.
    Added some logging to try and get better diagnostics on this error from UKMO.

--- test/test_results.py
+++ test/test_results.py
@@ -170,3 +170,20 @@ def test_shards_constrain3():
     for j in searchContext.search():
         print j.download_url, j.checksum, j.checksum_type, j.size
 
+
+def test_shards_constrain4():
+    # Regression test for issue #8 reported by ian.edmond@metoffice.gov.uk
+    conn = SearchConnection('http://pcmdi9.llnl.gov/esg-search',distrib=True)
+
+    ctx = conn.new_context(query='cmip5.output1.BCC.bcc-csm1-1-m.historical.mon.atmos.Amon.r1i1p1.v20120709')
+    s = ctx.search()
+
+    ds = s[0]
+
+    publicationDataset, server = ds.dataset_id.split('|')
+    print publicationDataset, server, ds.json['replica']
+    
+    searchContext = ds.file_context()
+    searchContext=searchContext.constrain(variable='tas')
+    for j in searchContext.search():
+        print j.download_url, j.checksum, j.checksum_type, j.size
