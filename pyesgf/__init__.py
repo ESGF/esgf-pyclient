b6ea57781c69f6cb3123f0529658f52438d59e1a
CEDA <CEDA@test.com>
2017-01-03 09:17:44 +0000

    Removed old TODO comments.

--- pyesgf/__init__.py
+++ pyesgf/__init__.py
@@ -4,12 +4,3 @@ Top-level package for Python interface to ESGF services.
 """
 
 __version__ = '0.1.7'
-
-#!TODO: ResultFormatter class.  process response json to specialise the result json.  Default is None
-#!TODO: pipe results to new process.  Command-line interface.
-#!TODO: Helper methods for "get opendap" "get download urls"
-
-    
-    
-
-    
