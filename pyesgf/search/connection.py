449a3581baed152b60786706667801660129a1d5
Frederic Laliberte <laliberte.frederic@gmail.com>
2017-01-10 11:31:59 -0500

    Use requests and requests_cache for handling queries.

--- pyesgf/search/connection.py
+++ pyesgf/search/connection.py
@@ -19,20 +19,23 @@ perform a search create a :class:`SearchConnection` instance then use
 
 """
 
+import requests
+import requests_cache
+from sqlite3 import DatabaseError
+import datetime
+import os
+
+import re
 from six.moves.urllib.request import urlopen
 from six.moves.urllib.error import HTTPError
 from six.moves.urllib.parse import urlparse
-import six
-import codecs
-import json
-import re
 
 import warnings
 import logging
 
-from .context import DatasetSearchContext
-from .consts import RESPONSE_FORMAT, SHARD_REXP
-from .exceptions import EsgfSearchException
+from pyesgf.search.context import DatasetSearchContext
+from pyesgf.search.consts import RESPONSE_FORMAT, SHARD_REXP
+from pyesgf.search.exceptions import EsgfSearchException
 from pyesgf.multidict import MultiDict
 from pyesgf.util import urlencode
 
@@ -49,24 +52,36 @@ class SearchConnection(object):
     :ivar distrib: Boolean stating whether searches through this connection are
         distributed.  I.e. whether the Search service distributes the query to
         other search peers.
-
+    :ivar cache: Path to `sqlite` cache file. Cache expires every hours.
+    :ivar timeout: Time (in seconds) before query returns an error.
+    :ivar expire_after: Time delta after cache expires
+    :ivar session: requests.session object. optional.
+    :ivar verify: boolean, determines if query should be sent over a verified
+                  channel.
     """
     # Default limit for queries.  None means use service default.
     default_limit = None
 
-    def __init__(self, url, distrib=True, context_class=None):
+    def __init__(self, url, distrib=True, cache=None, timeout=120,
+                 expire_after=datetime.timedelta(hours=1),
+                 session=None, verify=True, context_class=None):
         """
         :param context_class: Override the default SearchContext class.
 
         """
         self.url = url
         self.distrib = distrib
+        self.cache = cache
+        self.expire_after = expire_after
+        self.timeout = timeout
+        self.verify = verify
+        self._passed_session = session
 
         # Check URL for backward compatibility
         self.__check_url()
 
-        # _available_shards stores all available shards once retrieved from the
-        # server.
+        # _available_shards stores all available shards once
+        # retrieved from the server.
         # A value of None means they haven't been retrieved yet.
         # Once set it is a dictionary {'host': [(port, suffix), ...], ...}
         self._available_shards = None
@@ -75,6 +90,33 @@ class SearchConnection(object):
             self.__context_class = context_class
         else:
             self.__context_class = DatasetSearchContext
+
+    def open(self):
+        if (isinstance(self._passed_session, requests.Session) or
+            isinstance(self._passed_session,
+                       requests_cache.core.CachedSession)):
+            self.session = self.passed_session
+        else:
+            self.session = create_single_session(
+                                        cache=self.cache,
+                                        expire_after=self.expire_after)
+        return
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+        return
+
+    def close(self):
+        # Close the session
+        if not (isinstance(self._passed_session, requests.Session) or
+                isinstance(self._passed_session,
+                           requests_cache.core.CachedSession)):
+            self.session.close()
+        return
 
     def __check_url(self):
         """
@@ -100,21 +142,25 @@ class SearchConnection(object):
 
     def send_search(self, query_dict, limit=None, offset=None, shards=None):
         """
-        Send a query to the "search" endpoint.  See :meth:`send_query()` for
-                                                details.
+        Send a query to the "search" endpoint.
+        See :meth:`send_query()` for details.
 
         :return: The json document for the search results
 
         """
         full_query = self._build_query(query_dict, limit, offset, shards)
-        response, reader = self._send_query('search', full_query)
-        ret = json.load(reader(response))
+        self.open()
+        response = self._send_query('search', full_query)
+        ret = response.json()
+        response.close()
+        self.close()
+
         return ret
 
     def send_wget(self, query_dict, shards=None):
         """
-        Send a query to the "search" endpoint.  See :meth:`send_query()` for
-                                                details.
+        Send a query to the "search" endpoint.
+        See :meth:`send_query()` for details.
 
         :return: A string containing the script.
 
@@ -125,8 +171,11 @@ class SearchConnection(object):
         if 'format' in full_query:
             del full_query['format']
 
-        response, reader = self._send_query('wget', full_query)
-        script = reader(response).read()
+        self.open()
+        response = self._send_query('wget', full_query)
+        script = response.text
+        response.close()
+        self.close()
 
         return script
 
@@ -136,7 +185,7 @@ class SearchConnection(object):
         instances.
 
         :param full_query: dictionary of query string parameers to send.
-        :return: the urllib2 response object from the query and a reader object
+        :return: the requests response object from the query.
 
         """
 
@@ -145,25 +194,22 @@ class SearchConnection(object):
         query_url = '%s/%s?%s' % (self.url, endpoint, urlencode(full_query))
         log.debug('Query request is %s' % query_url)
 
-        try:
-            response = urlopen(query_url)
-        except HTTPError as err:
-            log.warn("HTTP request received error code: %s" % err.code)
-            if err.code == 400:
-                errors = set(re.findall("Invalid HTTP query parameter=(\w+)",
-                                        str(err.fp.read())))
+        response = self.session.get(query_url, verify=self.verify,
+                                    timeout=self.timeout)
+        if response.status_code == 400:
+            # If error code 400, use urllib to find the errors:
+            try:
+                response = urlopen(query_url)
+            except HTTPError as err:
+                errors = set(re.findall("Invalid HTTP query "
+                                        "parameter=(\w+)",
+                             err.fp.read()))
                 content = "; ".join([e for e in list(errors)])
                 raise Exception("Invalid query parameter(s): %s" % content)
-            else:
-                raise Exception("Error returned from URL: %s" % query_url)
 
-        if six.PY3:
-            response_encoding = response.headers.get_content_charset()
-        else:
-            response_encoding = response.headers.getparam('charset')
-        reader = codecs.getreader(response_encoding)
-
-        return response, reader
+        # Raise if query was unsucessful:
+        response.raise_for_status()
+        return response
 
     def _build_query(self, query_dict, limit=None, offset=None, shards=None):
         if shards is not None:
@@ -197,6 +243,7 @@ class SearchConnection(object):
             'shards': shard_str,
         })
         full_query.extend(query_dict)
+        print(full_query)
 
         # Remove all None valued items
         full_query = MultiDict(item for item in full_query.items()
@@ -303,3 +350,29 @@ def query_keyword_type(keyword):
         return 'system'
     else:
         return 'facet'
+
+
+def create_single_session(cache=None, expire_after=datetime.timedelta(hours=1),
+                          **kwargs):
+    """
+    Simple helper function to start a requests or requests_cache session.
+
+    cache, if specified is a filename to a threadsafe sqlite database
+    expire_after specifies how long the cache should be kept
+    """
+    if cache is not None:
+        try:
+            session = (requests_cache.core
+                       .CachedSession(cache,
+                                      expire_after=expire_after))
+        except DatabaseError:
+            # Corrupted cache:
+            try:
+                os.remove(cache)
+            except:
+                pass
+            session = (requests_cache.core
+                       .CachedSession(cache, expire_after=expire_after))
+    else:
+        session = requests.Session()
+    return session
