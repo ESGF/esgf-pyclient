d377c27f2d9d82219b8cdb8e6c115ae56f6dc78a
Alan Iwi <alan.iwi@stfc.ac.uk>
2021-07-25 20:52:07 +0100

    update hostname of CEDA SLCS server

--- tests/test_logon.py
+++ tests/test_logon.py
@@ -20,7 +20,7 @@ except (ImportError, SyntaxError):
 TEST_USER = os.environ.get('USERNAME')
 TEST_PASSWORD = os.environ.get('PASSWORD')
 TEST_OPENID = os.environ.get('OPENID')
-TEST_MYPROXY = 'slcs1.ceda.ac.uk'
+TEST_MYPROXY = 'slcs.ceda.ac.uk'
 
 
 TEST_DATA_DIR = op.join(op.dirname(__file__), 'data')
